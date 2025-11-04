54cc08a1dee5c9c68ea592920d84f59374453c26
Darren Rozario <drozari2@terpmail.umd.edu>
2025-11-04 18:38:36 -0500

    added team_members.py

---
+++ team_members.py
@@ -0,0 +1,19 @@
+""" A module that outputs each team member's name and message. """
+
+def main():
+    """ Displays the team members' names and messages in the console through a 
+    list of dictionaries.
+    
+    Side effects:
+        Prints the list in a format in the console. 
+    """
+    team = [
+        {"Name": "Darren Rozario", "Message": "Hi, my name is Darren!"}
+        # Team members add their messages here
+    ]
+    
+    for members in team:
+        print(f"{members['Name']}: {members['Message']}")
+    
+if __name__ == "__main__": 
+    main()
